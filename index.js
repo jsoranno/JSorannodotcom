aefc89bc54f229df02aa6e06c8357d2f55f86fbe
Juanita Soranno <juanitasoranno@gmail.com>
2016-10-29 22:59:27 -0400

    Adjusting js files

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by juanitasoranno on 10/29/16.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+});
